69372f5a6f606cc8b4bd42b93fb9410ed980939e
TFMV <mcgeehan@gmail.com>
2024-05-14 10:03:48 -0500

    Under Construction

--- db/connection.go
+++ db/connection.go
@@ -7,16 +7,12 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-// GenerateConnString constructs a PostgreSQL connection string.
-func GenerateConnString(host, port, user, password, dbname string, isUnixSocket bool) string {
-	if isUnixSocket {
-		return fmt.Sprintf("postgres://%s:%s@/%s?host=%s", user, password, dbname, host)
-	}
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", user, password, host, port, dbname)
-}
-
-// Connect establishes a connection to the database using a connection string.
 func Connect(ctx context.Context, host, port, user, password, dbname string, isUnixSocket bool) (*pgx.Conn, error) {
-	connStr := GenerateConnString(host, port, user, password, dbname, isUnixSocket)
+	var connStr string
+	if isUnixSocket {
+		connStr = fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbname)
+	} else {
+		connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	}
 	return pgx.Connect(ctx, connStr)
 }
